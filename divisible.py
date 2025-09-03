ad48c7c0628a9558ac1cf11bb90d76218c07c0a1
Bhavana <bhavanabondili2005@gmail.com>
2025-09-03 15:24:09 +0530

    Delete divisible.py

--- divisible.py
+++
@@ -1,8 +0,0 @@
-#check if a number is divisible by 5 and 11 or not
-def divisibility(a):
-    if a%5==0 and a%11==0:
-        print(a, "is divisible by both 5 and 11")
-    else:
-        print(a, " is not divisible by either 5 or 11")
-divisibility(55)
-divisibility(22)
