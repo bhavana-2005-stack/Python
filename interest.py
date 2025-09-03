6ca7e59955e9cde0806fa346263cfe7da302739f
Bhavana <bhavanabondili2005@gmail.com>
2025-09-03 15:27:17 +0530

    Delete interest.py

--- interest.py
+++
@@ -1,8 +0,0 @@
-#find simple interest and total amount
-p=int(input("Enter principle amount"))
-r=int(input("enter rate of interest"))
-t=int(input("enter total number of months"))
-si=(p*t*r)/100
-amt=p+si
-print("simple interest",si)
-print("total amount",amt)
