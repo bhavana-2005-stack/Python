81bdd84ba9c5e7fc60ada680e5f5999b0a8b3464
Bhavana <bhavanabondili2005@gmail.com>
2025-09-03 15:29:00 +0530

    Delete rec.py

--- rec.py
+++
@@ -1,7 +0,0 @@
-#area and circumference of rectangle
-l=int(input("Enter length"))
-b=int(input("enter breadth"))
-area=l*b
-circumference=2*(l+b)
-print("area =" ,area)
-print("circumference =" ,circumference)
